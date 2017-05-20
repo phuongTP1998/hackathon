28c52abe9cddbaa9681918935b9ffc655cc08bba
Rexx0r <minhh.nguyen.97@gmail.com>
2017-05-20 21:27:06 +0700

    fix win scene 2

--- PhysicsDemo/src/physicsdemo/gameScenes/Level1Scene.java
+++ PhysicsDemo/src/physicsdemo/gameScenes/Level1Scene.java
@@ -56,7 +56,7 @@ public class Level1Scene implements GameScenes {
         new Ground(new GameRect(5100, 350, 100, 50), new SpriteRenderer("res/ground/ground001.png"), null);
         new Ground(new GameRect(5450, 450, 100, 50), new SpriteRenderer("res/ground/ground001.png"), null);
         new Ground(new GameRect(5800, 500, 400, 200), new SpriteRenderer("res/ground/ground111.png"), null);
-        new MilkBottle(new GameRect(6000,500,50,100),new SpriteRenderer("res/milkbottle/milkbottle.png"));
+        new MilkBottle(new GameRect(6000,400,50,100),new SpriteRenderer("res/milkbottle/milkbottle.png"));
 
         image = Utils.loadImage("res/SCREEN/background3.png");
         EnemyController enemyController1 = new EnemyController(new GameRect(900, -10, 50, 60), new SpriteRenderer("res/Dragon-Scaled/Minh/run-left-1.png"));
